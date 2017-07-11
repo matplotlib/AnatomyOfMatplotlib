d633b769aeb5c78ed789fdeaa0f96d047753cbef
Benjamin Root <ben.v.root@gmail.com>
2017-07-11 11:34:33 -0400

    Erm... 5-pointed star

--- solutions/5.1-goldstar.py
+++ solutions/5.1-goldstar.py
@@ -4,7 +4,7 @@ from matplotlib.collections import StarPolygonCollection
 fig, ax = plt.subplots(1, 1)
 
 offsets = zip([0.2, 0.4, 0.6, 0.8], [0.5] * 4)
-collection = StarPolygonCollection(4,
+collection = StarPolygonCollection(5,
                                    offsets=offsets,
                                    transOffset=ax.transData,
                                    facecolors=['gold'],
